d934aaabebd0743a219fca0563195e7dd9d4b5cf
aaron-rf <aaron.rubio.fernandez@gmail.com>
2024-04-15 08:35:35 +0200

    added parametrization and fixtures to pytest examples

--- test_hello.py
+++ test_hello.py
@@ -6,6 +6,19 @@ def test_add():
     assert result == 1 + 2
 
 
-def test_multiply():
+@pytest.mark.parametrize("a, b, result_expected", [(2, 4, 8), (3, 0, 0), (-1, 2, -2)])
+def test_multiply(a, b, result_expected):
     result = multiply(2, 4)
-    assert result == 2 * 4
+    assert result == result_expected
+
+
+@pytest.fixture
+def get_random_prime():
+    from random import choice
+    list_primes = [1, 3, 5, 7, 11, 13]
+    current_prime = choice(list_primes)
+    return current_prime
+
+
+def test_divisible_by_2(get_random_prime):
+    assert get_random_prime % 2 != 0
